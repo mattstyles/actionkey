3ba7ac8618f719dc3c0c15a2d9cc04298719ed01
Matt Styles <matt@veryfizzyjelly.com>
2016-07-05 09:11:37 +0100

    add tests

---
+++ test.js
@@ -0,0 +1,38 @@
+
+const tape = require('tape')
+const actionkey = require('./')
+
+let output = {
+  'FOO': 'FOO'
+}
+
+let nsOutput = {
+  'FOO': 'BAR_FOO'
+}
+
+tape('It should accept an array of keys and return an object', t => {
+  t.plan(1)
+
+  t.deepEqual(output, actionkey(['FOO']))
+})
+
+tape('It should accept an objects and return an object', t => {
+  t.plan(1)
+
+  t.deepEqual(output, actionkey({'FOO': null}))
+})
+
+tape('It should accept a namespace for values', t => {
+  t.plan(2)
+
+  t.deepEqual(nsOutput, actionkey('BAR', ['FOO']))
+  t.deepEqual(nsOutput, actionkey('BAR', {'FOO': null}))
+})
+
+tape('It should return a frozen object', t => {
+  t.plan(1)
+
+  let action = actionkey(['FOO'])
+  action.FOO = 'BAR'
+  t.deepEqual(output, actionkey(['FOO']))
+})
